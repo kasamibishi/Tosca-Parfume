4b715dd0f4771a1b8e1369b6a84c5569f28475a5
Kasam Ibishi <115446471+kasamibishi@users.noreply.github.com>
2026-08-01 18:07:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+function normalizeString(str) {
+    return str.toLowerCase().replace(/[^a-z0-9]/g, '');
+}
+
 const searchBar = document.getElementById('search-bar');
 const productCards = document.querySelectorAll('.product-card');
 
 searchBar.addEventListener('input', (e) => {
-    const searchQuery = e.target.value.toLowerCase();
+    
+    const searchQuery = normalizeString(e.target.value);
 
     productCards.forEach(card => {
-        const productText = card.textContent.toLowerCase();
+        const productName = card.querySelector('.product-name').textContent;
+        
+        const normalizedProductName = normalizeString(productName);
 
-        if (productText.includes(searchQuery)) {
+        if (normalizedProductName.includes(searchQuery)) {
             card.style.display = 'block';
         } else {
             card.style.display = 'none';
@@ -25,19 +32,34 @@ const mobileMenu = document.getElementById('mobile-menu');
 const closeMenuBtn = document.getElementById('close-menu');
 const menuOverlay = document.getElementById('menu-overlay');
 
-// Open Menu
 menuIcon.addEventListener('click', () => {
     mobileMenu.classList.add('open-menu');
     menuOverlay.classList.add('active');
-    document.body.style.overflow = 'hidden'; // Prevents background scrolling
+    document.body.style.overflow = 'hidden';
 });
 
-// Close Menu Function
 function closeMenu() {
     mobileMenu.classList.remove('open-menu');
     menuOverlay.classList.remove('active');
-    document.body.style.overflow = 'auto'; // Restores scrolling
+    document.body.style.overflow = 'auto';
 }
 
 closeMenuBtn.addEventListener('click', closeMenu);
 menuOverlay.addEventListener('click', closeMenu);
+
+const scrollTopBtn = document.getElementById('scroll-top-btn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('show-btn');
+  } else {
+    scrollTopBtn.classList.remove('show-btn');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
