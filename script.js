18e5dfb8ab2d1a549b84c22c54b9e1c62d8306a2
Kasam Ibishi <115446471+kasamibishi@users.noreply.github.com>
2026-08-01 05:35:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ function generateWhatsAppLink(phoneNumber, productName, price) {
     const text = `Përshëndetje! Dëshiroj të porosis: ${productName} (${price} MKD).`;
     return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`;
 }
+
+const menuIcon = document.getElementById('menu-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenuBtn = document.getElementById('close-menu');
+const menuOverlay = document.getElementById('menu-overlay');
+
+// Open Menu
+menuIcon.addEventListener('click', () => {
+    mobileMenu.classList.add('open-menu');
+    menuOverlay.classList.add('active');
+    document.body.style.overflow = 'hidden'; // Prevents background scrolling
+});
+
+// Close Menu Function
+function closeMenu() {
+    mobileMenu.classList.remove('open-menu');
+    menuOverlay.classList.remove('active');
+    document.body.style.overflow = 'auto'; // Restores scrolling
+}
+
+closeMenuBtn.addEventListener('click', closeMenu);
+menuOverlay.addEventListener('click', closeMenu);
